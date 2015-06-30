6aa340b03c9af661da0e6d3a9d3c8e0068e33853
Jess <jessi0vanb@gmail.com>
2015-06-30 11:05:42 -0700

    Initialize and create sensor thread

--- QNX_Tutorial.c
+++ QNX_Tutorial.c
@@ -9,6 +9,8 @@
 #include <stdio.h>		//for standard i/o (printf, perror)
 #include <pthread.h>	//for using threads
 #include <sched.h>		//for adding priorities to threads
+//Methods:
+void *sense(void* arg);
 
 /* --------------------------------------------------------------------------
  * ---------------------------------- MAIN ----------------------------------
@@ -16,7 +18,26 @@
 
 int main(int argc, char *argv[]) {
 
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+	//Initialize thread ID's:
+	pthread_t sensorThread;
+
+	//Start threads:
+	pthread_create(&sensorThread, NULL, sense, NULL);
+
+	printf("Goodbye World!\n");
 
 	return EXIT_SUCCESS;
+}
+
+/* --------------------------------------------------------------------------
+ * --------------------------------- THREADS --------------------------------
+ * -------------------------------------------------------------------------- */
+
+/*
+ * Thread to sense if there has been a change in state.
+ */
+void *sense(void* arg) {
+	return NULL;
 }
